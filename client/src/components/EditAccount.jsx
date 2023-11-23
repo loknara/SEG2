c4cb06316bddd612bd616f52ac5d4d23c247003b
anishdasti <anishkdasti@gmail.com>
2023-11-22 20:37:05 -0600

    Updated profile and edit account pages

--- client/src/components/EditAccount.jsx
+++ client/src/components/EditAccount.jsx
@@ -1,75 +1,73 @@
 /* Created by Anish Dasti */
+/* 
+This page allows to change the password. Can test this by logging in and entering the current password and a new password. 
+After signing out, logging in with the old password won't work, and user must use the new password that they changed it to. 
+
+Can use this login to test, and change password in edit account page:
+Email: test2@gmail.com
+Password: password
+*/
 
 import React, { useState } from 'react';
 import './EditAccount.css';
+import { useAuth } from '../contexts/AuthContext';
+import { getAuth, updatePassword } from 'firebase/auth';
 
-/*  function EditAccount() {
-    return (
-        <div>
-            <div className="header">
-                <h1>Edit Account Details</h1>
-            </div>
+function EditAccount() {
+  const { currentUser } = useAuth();
 
-        </div>
-       
-      );
-    }*/
+  const [password, setPassword] = useState('');
+  const [currentPassword, setCurrentPassword] = useState('');
 
-    function EditAccount() {
-      const [email, setEmail] = useState('test2@gmail.com');
-      const [username, setUsername] = useState('test2@gmail.com');
-      const [password, setPassword] = useState('xxxxxxxx');
-    
-      const handleSaveChanges = () => {
-        // Implement logic to save changes to the backend or update state
-        console.log('Changes saved!');
-      };
-    
-      return (
-        <div className="currensee-container">
-          <div className="header">
-            <h1>EDIT ACCOUNT DETAILS</h1>
-          </div>
-    
-          <div className="edit-profile-details">
-            <div className="edit-detail">
-              <label htmlFor="email">Email:</label>
-              <input
-                type="text"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="edit-detail">
-              <label htmlFor="username">Username:</label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="edit-detail">
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-          </div>
-    
-          <div className="button-container">
-            <button className="save-button" onClick={handleSaveChanges}>
-              Save Changes
-            </button>
-          </div>
-        </div>
-      );
+  const handleChangePassword = async () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    try {
+      // Update password
+      await updatePassword(user, password);
+
+      alert('Password updated successfully!');
+    } catch (error) {
+      console.error('Error updating password', error.message);
+      alert('Error updating password. Please check your current password and try again. (Password must be at least 6 characters)');
     }
+  };
 
+  return (
+    <div className="currensee-container">
+      <div className="header">
+        <h1>EDIT ACCOUNT DETAILS</h1>
+      </div>
+
+      <div className="edit-profile-details">
+        <div className="edit-detail">
+          <label htmlFor="currentPassword">Current Password:</label>
+          <input
+            type="password"
+            id="currentPassword"
+            value={currentPassword}
+            onChange={(e) => setCurrentPassword(e.target.value)}
+          />
+        </div>
+        <div className="edit-detail">
+          <label htmlFor="password">New Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="button-container">
+        <button className="save-button" onClick={handleChangePassword}>
+          Save Changes
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default EditAccount;
-
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,10 +1,22 @@
-/* Created by Anish Dasti */
+/* Created by Anish Dasti 
+
+Once logged in, the profile page will update and display the user's profile details.
+
+Can use this login:
+Email: test@gmail.com
+Password: password
+
+And change email to test2@gmail.com to test the change
+*/
 
 import React from 'react';
 import './Profile.css';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
   function Profile() {
+    const { currentUser } = useAuth();
+
     return (
         <div className="currensee-container">
             <div className="header">
@@ -14,10 +26,10 @@ import { Link } from 'react-router-dom';
             
             <div className="profile-details">
               <div className="detail">
-                <strong>Email:</strong> test@gmail.com
+                <strong>Email:</strong> {currentUser ? currentUser.email : ""}
               </div>
               <div className="detail">
-                <strong>Username:</strong> test@gmail.com
+                <strong>Username:</strong> {currentUser ? currentUser.email : ""}
               </div>
               <div className="detail">
                 <strong>Password:</strong> xxxxxxxx
@@ -44,49 +56,3 @@ import { Link } from 'react-router-dom';
 
 export default Profile;
 
-/* 
-import React from 'react';
-import './Profile.css';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
-import { getAuth } from 'firebase/auth';
-
-function Profile() {
-  const { currentUser } = useAuth();
-
-  return (
-    <div className="currensee-container">
-      <div className="header">
-        <h1>PROFILE</h1>
-      </div>
-
-      <div className="profile-details">
-        <div className="detail">
-          <strong>Email:</strong> {currentUser ? currentUser.email : ""}
-        </div>
-        <div className="detail">
-          <strong>Username:</strong> {currentUser ? currentUser.displayName : ""}
-        </div>
-        <div className="detail">
-          <strong>Password:</strong> xxxxxxxx
-        </div>
-      </div>
-
-      <div className="button-container">
-        <Link to="/Profile/EditAccount">
-          <button className="edit-button">Edit Account Details</button>
-        </Link>
-        <button className="advanced-button" onClick={() => handleAdvancedSettingsClick()}>
-          Advanced Settings
-        </button>
-      </div>
-    </div>
-  );
-}
-
-function handleAdvancedSettingsClick() {
-  // Navigate to the advanced settings page
-}
-
-export default Profile;
-*/
